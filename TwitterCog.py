fdf7b2f539d412e6b651526a0e0e93cc1f6379c2
Tallerik <erik.klasse@gmail.com>
2020-02-01 13:46:59 +0100

    Updated TwitterCog.py - Added Security feature via DC role Updated settings.ini - Added ID of DC role

--- TwitterCog.py
+++ TwitterCog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from twython import Twython
-from Data import TwitterLoginInfos, TokenConfig
+from Data import TokenConfig, RolesConfig
 
 
 class TwitterCog(commands.Cog):
@@ -13,9 +13,19 @@ class TwitterCog(commands.Cog):
         cred = tokens.get_twitter_tokens()
         self.twitter = Twython(cred.get_apikey(),cred.get_apisecret(),cred.get_accesstoken(),cred.get_accesstokensecret())
 
-
     @commands.command()
     async def twitterit(self, obj, msg):
+        roles = RolesConfig()
+        twitterrole = roles.get_role("Twitter")
+        if obj.guild is None:
+            return
+        if obj.guild.get_role(int(twitterrole)) is None:
+            print("Role Twitter with id " + twitterrole + " not found")
+            return
+        if obj.guild.get_role(int(twitterrole)) not in obj.message.author.roles:
+            await obj.channel.send('Du hast keine Rechte, das Twitterfeature zu nutzen.')
+            return
+
         message = obj.message.content
         message = message.replace(self.bot.command_prefix + "twitterit ", "")
 
